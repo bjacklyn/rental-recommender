a8f7dfdef5b8aa00e528a44d8fa7d286349970a8
Brandon Jacklyn <brandonjacklyn@live.ca>
2024-10-14 14:44:56 -0700

    Add trace instrumentation to logs in myapp

--- myapp/app.py
+++ myapp/app.py
@@ -2,12 +2,15 @@ from flask import Flask, jsonify, render_template, Blueprint
 from flask_restx import Api, Resource, Swagger
 from flask_restx.apidoc import apidoc
 
+import logging
+
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource as OpenTelemetryResource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 @apidoc.add_app_template_global
 def swagger_static(filename):
@@ -52,12 +55,18 @@ trace.get_tracer_provider().add_span_processor(span_processor)
 
 # Instrument the Flask app
 FlaskInstrumentor().instrument_app(app)
+LoggingInstrumentor().instrument(set_logging_format=True)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @api.route('/api/hello')
 class HelloWorld(Resource):
     def get(self):
         """Returns a hello world message"""
         with tracer.start_as_current_span("hello_world_trace"):
+            logger.info("Starting trace for /api/hello endpoint. This msg will have the trace info embedded in it!")
             return jsonify({"message": "Hello, World!"})
 
 if __name__ == '__main__':
